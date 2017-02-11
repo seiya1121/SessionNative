548d327271d772f25b3386cac9e1d8ef10ebfb27
seiya kawamura <seiya.kawamura@gmail.com>
2017-02-11 22:01:40 +0900

    fix gitignore

--- index.ios.js
+++ index.ios.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
-import * as appReducer from './src/reducers/app.js';
+import * as app from './src/reducers/app.js';
 import App from './src/components/app.js';
 
-const reducer = combineReducers(appReducer);
+const reducer = combineReducers(app);
 const store = createStore(reducer);
 
 class Root extends Component {
